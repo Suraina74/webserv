58a04bce485d470c32738011fb98a9908c155374
Alisha Soedamah <ksoedama@f0r6s8.codam.nl>
2026-07-02 16:43:28 +0200

    init project structure

---
+++ src/tick.cpp
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   tick.cpp                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: ksoedama <ksoedama@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2026/07/02 15:56:03 by ksoedama      #+#    #+#                 */
+/*   Updated: 2026/07/02 16:42:48 by ksoedama      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//poll 
+// check readyness of the sockets?? before calling read write on them
+//sockets
+//do read/write calls on sockets???
