48d23537030004d2a6d676fc0b959d77103f0522
Alisha Soedamah <ksoedama@f1r2s2.codam.nl>
2026-07-21 18:15:33 +0200

    made functions to make the main smaller

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 int	eventLoop(int *sockfd)
 {
 	EventLoop poll_fds;
+	char index_page_txt[2048];
 
 	memset(&poll_fds.fds, 0, sizeof(poll_fds.fds));
 	while (1)
@@ -16,30 +17,65 @@ int	eventLoop(int *sockfd)
 		if (new_fd == -1)
 		{
 			perror("accept");
-			continue;
+			return (1);
 		}
 		poll(&poll_fds.fds, 1, 100);
-		char index_page_txt[2048];
-
-		// if (mypoll.revents & POLLIN)
-		// {
+		// if (poll_fds.fds.revents & POLLIN)
+		{
 			int open_index = open("www/index.html", O_RDONLY);
 			ssize_t n = read(open_index, index_page_txt, 2048);
 			index_page_txt[n] = '\0';
 			std::cout << index_page_txt << std::endl;
-		// }
-		// else if (mypoll.revents & POLLOUT) {
+		}
+		// else if (poll_fds.fds.revents & POLLOUT)
+		{
 			send(new_fd, index_page_txt, 2048, 0);
-		// }
-
+		}
 	}
+	return (0);
+}
+
+int createSockAddr(int *sockfd, struct addrinfo *result)
+{
+	struct addrinfo info;
+	struct addrinfo *ptr;
+
+	memset(&info, 0, sizeof(info));
+	info.ai_family = AF_INET;
+	info.ai_socktype = SOCK_STREAM;
+	if (getaddrinfo("127.0.0.1", "8080", &info, &result) != 0)
+	{
+		std::cout << "getaddrinfo failed\n";
+		return (1);
+	}
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next)
+	{
+		*sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+		if (*sockfd == -1)
+		{
+			std::cout << "socket failure\n";
+			return (1);
+		}
+		int on = true;
+		if ((setsockopt(*sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) == -1)
+		{
+			std::cout << "socket option failure\n";
+			return (1);
+		}
+		std::cout << "connected to the host!\n";
+		if (bind(*sockfd, result->ai_addr, result->ai_addrlen) == -1)
+		{
+			std::cout << "bind failure\n";
+			return (1);
+		}
+		break ;
+	}
+	return (0);
 }
 
 int	main(int ac, char **av)
 {
-	struct addrinfo *result;
-	struct addrinfo info;
-	struct addrinfo *ptr;
+	struct addrinfo *result = nullptr;
 	int sockfd = 0;
 
 	(void)av;
@@ -48,39 +84,14 @@ int	main(int ac, char **av)
 		std::cout << "usage: ./webserv config" << std::endl;
 		return (1);
 	}
-	memset(&info, 0, sizeof(info));
-	info.ai_family = AF_INET;
-	info.ai_socktype = SOCK_STREAM;
-	if (getaddrinfo("127.0.0.1", "8080", &info, &result) != 0){
-		std::cout << "getaddrinfo failed\n";
-		return 1;
-	}
-
-	for (ptr = result; ptr != NULL; ptr = ptr->ai_next)
-	{
-		sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-		if (sockfd == -1){
-			std::cout << "socket failure\n";
-			return 1;
-		}
-		int on = true;
-		if ((setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) == -1)
-		{
-			std::cout << "socket option failure\n";
-			return 1;
-		}
-		std::cout << "connected to the host!\n";
-		if (bind(sockfd, result->ai_addr, result->ai_addrlen) == -1){
-			std::cout << "bind failure\n";
-			return 1;
-		}
-		break ;
-	}
+	if (createSockAddr(&sockfd, result))
+		return (1);
 	freeaddrinfo(result);
 	if (listen(sockfd, 10) != 0){
-		std::cout << "listen failure\n";
-		return 1;
+		perror("listen");
+		return (1);
 	}
-	eventLoop(&sockfd);
+	if (eventLoop(&sockfd))
+		return (1);
 	return (0);
 }
